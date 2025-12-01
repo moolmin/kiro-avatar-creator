9d2cd34a3d2a7ef6fa419934f38ee6f1e2917a80
sumin <zzmzzm0065@gmail.com>
2025-12-01 11:26:39 +0900

    fix: change mobile save button icon from bookmark to download

--- components/SaveButton.tsx
+++ components/SaveButton.tsx
@@ -153,7 +153,8 @@ export default function SaveButton({ svgRef, className = '' }: SaveButtonProps) 
             fill="currentColor"
             aria-hidden="true"
           >
-            <path d="M5 4a2 2 0 012-2h6a2 2 0 012 2v14l-5-2.5L5 18V4z" />
+            <path d="M10.75 2.75a.75.75 0 00-1.5 0v8.614L6.295 8.235a.75.75 0 10-1.09 1.03l4.25 4.5a.75.75 0 001.09 0l4.25-4.5a.75.75 0 00-1.09-1.03l-2.955 3.129V2.75z" />
+            <path d="M3.5 12.75a.75.75 0 00-1.5 0v2.5A2.75 2.75 0 004.75 18h10.5A2.75 2.75 0 0018 15.25v-2.5a.75.75 0 00-1.5 0v2.5c0 .69-.56 1.25-1.25 1.25H4.75c-.69 0-1.25-.56-1.25-1.25v-2.5z" />
           </svg>
           {/* Desktop: Text only */}
           <span className="hidden md:block text-sm font-semibold">
